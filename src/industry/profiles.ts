d34f4299415db9435fb243a59be457d1cfd8d54d
samdongpapa-create <samdongpapa@gmail.com>
2026-02-10 10:14:05 +0900

    Update profiles.ts

--- src/industry/profiles.ts
+++ src/industry/profiles.ts
@@ -2,13 +2,9 @@ export type IndustryProfile = {
   subcategory: string;
   vertical: string;
 
-  // 대표 키워드 템플릿
   coreKeywords: string[];
-
-  // 서비스/메뉴 키워드 사전
   serviceKeywords: string[];
 
-  // 상세설명 생성기
   descriptionTemplate: (ctx: {
     name: string;
     region: string;
@@ -17,12 +13,8 @@ export type IndustryProfile = {
     cta: string;
   }) => string;
 
-  // 찾아오는 길 템플릿
   directionsTemplate: (region: string) => string;
 
-  // 사진 평가 기준
   photoChecklist: string[];
-
-  // 금칙어 / 리스크 문구
   bannedPhrases: string[];
 };
